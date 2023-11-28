a3deae4accca7b060d99e4ef5ca3034b32c21e3a
subhankar <nathsubhankar007@gmail.com>
2023-11-28 11:47:11 +0530

    28-11-2023

---
+++ suvo_decimal_to_binary3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def decimal_to_binary(number):
+    str1=""
+    while number>0:
+        rem=number%2
+        str1=str(rem)+str1
+        number=number//2
+    return str1
+
+number=int(input("Enter a decimal number : "))
+print(f"binary of {number} is {decimal_to_binary(number)}")
+        
