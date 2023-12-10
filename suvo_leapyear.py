f1d4a4fa60e662dac1a3c964942e5c26cfaea1fc
Subhankar Nath <128243653+SUVO101@users.noreply.github.com>
2023-12-10 23:06:41 +0530

    Update suvo_leapyear.py

--- suvo_leapyear.py
+++ suvo_leapyear.py
@@ -7,3 +7,14 @@ print("leapyear - ",end=" ")
 for year in range(starting_year,ending_year+1):
     if year%4==0 and year%400==0 or year%100!=0:
         print(year,end=" ")
+
+#extra logic
+def is_year_leap(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
